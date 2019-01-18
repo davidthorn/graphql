b27b17d02c2d9bec410070934f52fa951715033f
David Thorn <david.thorn221278@googlemail.com>
2019-01-18 04:12:21 +0100

    saved

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import express, {} from 'express'
 import graphqlHTTP from 'express-graphql'
-import { SpaceX } from './MyGraphQlSchema'
+import { SpaceX } from './SpaceX'
 
 const app = express()
 
